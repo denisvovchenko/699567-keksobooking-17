a59e6948c7e4460be5100ebf7bb9039460338466
Denys <denisvovchenko96@mail.ru>
2019-06-13 23:52:06 +0200

    add creatPins function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 'use strict';
 
+var BODY = document.querySelector('body')
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var MAP = document.querySelector('.map');
 var MAP_WIDTH = MAP.offsetWidth;
+
+var MAP_DIMENSIONS = {
+  x: {
+    start: 0,
+    end: MAP_WIDTH,
+  },
+
+  y: {
+    start: 130,
+    end: 630,
+  },
+}
+
+var MAP_PINS = document.querySelector('.map__pins');
+
+var PIN_SIZE = {
+  width: parseInt( getPinProps('width') ),
+  height: parseInt( getPinProps('height') ),
+};
+
 var MOCKS_LENGTH = 8;
 var MOCKS = fillMocks(MOCKS_LENGTH);
 
 // временно
 MAP.classList.remove('map--faded');
+
+createPins();
+
+// Functions
 
 function fillMocks(count) {
   var mocks = [];
@@ -26,8 +51,8 @@ function fillMocks(count) {
       },
 
       location: {
-        x: getRandomNum(0, MAP_WIDTH),
-        y: getRandomNum(130, 630),
+        x: getRandomNum(PIN_SIZE.width, MAP_WIDTH - (PIN_SIZE.width)),
+        y: getRandomNum(MAP_DIMENSIONS.y.start + PIN_SIZE.height, MAP_DIMENSIONS.y.end),
       }
     };
   }
@@ -56,4 +81,42 @@ function getRandomNumsArrayWithoutRepeat(count) {
   }
 
   return randomNumbers;
+}
+
+function createPins() {
+  var pins = document.createDocumentFragment();
+
+  for (var i = 0; i < MOCKS_LENGTH; i++) {
+    var pin = document.querySelector('#pin')
+                      .content
+                      .querySelector('.map__pin')
+                      .cloneNode(true);
+    var pinPhoto = pin.querySelector('img');
+    var pinProps = MOCKS[i];
+    var pinCoordX = 'left: ' + pinProps.location.x + 'px;';
+    var pinCoordY = 'top: ' + pinProps.location.y + 'px;';
+
+    pin.style = pinCoordX + ' ' + pinCoordY;
+    pinPhoto.src = pinProps.author.avatar;
+    pinPhoto.alt = pinProps.offer.type;
+
+    pins.appendChild(pin);
+  }
+
+  MAP_PINS.appendChild(pins);
+}
+
+function getPinProps(prop) {
+  var element = document.querySelector('#pin')
+                        .content
+                        .querySelector('.map__pin')
+                        .cloneNode(true);
+
+  BODY.appendChild(element);
+
+  var value = window.getComputedStyle(element).getPropertyValue(prop);
+
+  BODY.removeChild(element);
+
+  return value ? value : '';
 }
